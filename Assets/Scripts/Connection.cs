d8abc9b535a0bf882c27df4b3cda13792f8a0e73
KS <kalle.sillanpaa@taitotalo.fi>
2024-05-29 10:48:30 +0300

    Update (connection test)
    Added ping test to test connection

--- Assets/Scripts/Connection.cs
+++ Assets/Scripts/Connection.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using System;
 
 
 public class Connection
-{                                                          
-    MongoClient mongoClient = new MongoClient("mongodb+srv://USERNAME:PASSWORD@cluster0.37ex2rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+{
+    MongoClient mongoClient = new MongoClient("mongodb+srv://USER:PASSWORD@cluster0.37ex2rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     public IMongoDatabase db;
     IMongoCollection<BsonDocument> collection;
 
-    //konstruktori (kutsutaan, kun olio Connection-luokasta luodaan kahdella string-parametrilla)
+    //konstruktori
     public Connection(string dbName, string dbCollection)
     {
         db = mongoClient.GetDatabase(dbName); //asetetaan muuttujaan parametrina saatu tietokannan nimi
         collection = db.GetCollection<BsonDocument>(dbCollection);
         Debug.Log($"Connection-olio luotiin tietokannalle {dbName}, collectionille {dbCollection}");
+
+        try
+        {
+            //Pingataan yhteys, jotta tiedet‰‰n, saatiinko yhteys vai ei:
+            var result = db.RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+            Debug.Log($"Ping OK to DB {dbName}");
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Cannot connect. Error: {e}");
+        }
     }
     
 }
